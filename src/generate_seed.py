f2f42801753cc27b2463ee2a8d865ae5942c0965
Clement549 <clementroure@orange.fr>
2022-09-30 23:11:14 +0200

    update

--- src/generate_seed.py
+++ src/generate_seed.py
@@ -1,43 +1,57 @@
+import secrets
 import hashlib
 import binascii
-import secrets
 print()
 
-entropy = secrets.token_hex(32)
-print("random generated entropy (in hexa): " + entropy, end="\n\n")
+# entropy
+print("Random Entropy in 128 bits: ")
+entropy_hex = secrets.token_hex(16)
+#entropy_hex = "656d338db7c217ad57b2516cdddd6d06"
 
-# convert hexa to bytes
-data = binascii.unhexlify(entropy)
+print(entropy_hex, end="\n")
+print()
 
-<<<<<<< HEAD
 # generate hash with sha256
 print("sha256: ")
 sha256 = hashlib.sha256(bytes.fromhex(entropy_hex)).hexdigest()
-=======
-h = hashlib.sha256(data).hexdigest()
-print("entropy after sha-256: " + h, end="\n\n")
->>>>>>> 6f6b2be2dd75fa5956dd998ffc0c597d6ea1a19c
 
-b = bin(int(binascii.hexlify(data),16))[2:].zfill(len(data)*8) + bin(int(h,16))[2:].zfill(256)[: len(data)* 8//32]
+print(sha256)
+print()
 
-print("132 bits: ")
+# generate seed by adding checksum
+print("seed (entropy + checksum): ")
+seed_hex = entropy_hex + sha256[0]
+
+print(seed_hex)
+print()
+
+# convert seed from hex to binary
+seed = bin(int(seed_hex, 16))[2:].zfill(8)
+if(len(seed) < 132):
+    d = 132 - len(seed)
+    for i in range(d):
+       seed = "0"+seed
+
+print("132 bits seed: ")
 i = 0
-for bit in b:
-    if i < 11:
+for bit in seed:
+    if i < 10:
        print(bit, end = '')
        i+=1
     else:
        i=0
-       print(" ", end = '')
+       print(bit + " ", end = '')
 print()
 
 with open("assets/words.txt", "r") as f:
          wordlist = [w.strip() for w in f.readlines()]
-print()
 
-seed = []
-for i in range(len(b)//11):
-    indx = int(b[11*i:11*(i+1)],2)
-    seed.append(wordlist[indx])
-print("12 mnemonic code:") 
-print(seed, end="\n\n") 
+print()
+words= []
+for i in range(len(seed)//11):
+    indx = int(seed[11*i:11*(i+1)],2)
+    words.append(wordlist[indx])
+print("12 mnemonic words:") 
+for word in words:
+    print(word, end=" ") 
+print("",end="\n\n")
