f3d93824fa808490e894f0f6946ccbda5627a587
Clement549 <clementroure@orange.fr>
2022-09-30 15:01:14 +0200

    main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import hashlib # for SHA256 computation
 import binascii # for conversion between Hexa and bytes
 import secrets
+print()
 
 entropy = secrets.token_hex(32)
-print("hexa:" + entropy)
+print("random generated entropy (in hexa): " + entropy, end="\n\n")
 
 data = entropy.strip() #cleaning of data
 data = binascii.unhexlify(data)
@@ -13,16 +14,27 @@ if len(data) not in [16, 20, 24, 28, 32]:
    )
 
 h = hashlib.sha256(data).hexdigest()
-print(h)
+print("entropy after sha-256: " + h, end="\n\n")
+
 b = bin(int(binascii.hexlify(data),16))[2:].zfill(len(data)*8) + bin(int(h,16))[2:].zfill(256)[: len(data)* 8//32]
-print(b)
+
+print("132 bits: ")
+i = 0
+for bit in b:
+    if i < 11:
+       print(bit, end = '')
+       i+=1
+    else:
+       i=0
+       print(" ", end = '')
+print()
 
 with open("assets/words.txt", "r") as f:
          wordlist = [w.strip() for w in f.readlines()]
+print()
 
 seed = []
 for i in range(len(b)//11):
     indx = int(b[11*i:11*(i+1)],2)
     seed.append(wordlist[indx])
-#print(entropy)
-print(seed) 
+print(seed, end="\n\n") 
